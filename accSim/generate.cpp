7dfff222880e29bfe1a0543c3acd02483e37e514
Jimmy Xin <jxin31415@gmail.com>
2022-05-26 17:24:31 -0500

    Thursday work

--- accSim/generate.cpp
+++ accSim/generate.cpp
@@ -4,16 +4,16 @@
 
 using namespace std;
 
+static const bool genCsv = true;
+static const bool genJson = true;
 
-bool genCsv = true;
-bool genJson = true;
+static const double T_STEP = .05;
+static const double T_TOT = 15;
 
-double T_STEP = .05;
-double T_TOT = 15;
-double stoppingDistanceMean = 10.0;
-double stoppingDistanceStdDev = 0.0;
-double vErrMean = 0.0;
-double vErrStdDev = 0.2;
+static const double stoppingDistanceMean = 10.0;
+static const double stoppingDistanceStdDev = 0.0;
+static const double vErrMean = 0.0;
+static const double vErrStdDev = 0.0;
 
 
 enum State {
@@ -22,51 +22,46 @@ enum State {
   CON
 };
 
-std::random_device rd;
-std::default_random_engine gen(rd());
-std::normal_distribution<double> stoppingDistanceDistr(stoppingDistanceMean, stoppingDistanceStdDev);
-std::normal_distribution<double> vErrDistr(vErrMean, vErrStdDev);
+random_device rd;
+default_random_engine gen(rd());
+normal_distribution<double> stoppingDistanceDistr(stoppingDistanceMean, stoppingDistanceStdDev);
+normal_distribution<double> vErrDistr(vErrMean, vErrStdDev);
 
-double epsilon = 10E-14;
+// double epsilon = 10E-14;
 
 class Robot {
-
 
   private:
   public:
 
-  double accMax = 4;
-  double decMax = -4;
-  double vMax = 15;
+  double accMax;
+  double decMax;
+  double vMax;
 
-  double target = 100;
-  double stoppingDistance = stoppingDistanceDistr(gen);
-
-  Robot(){}
+  double target;
+  double stoppingDistance;
 
   Robot(double _accMax, double _decMax, double _vMax, double _target){
     accMax = _accMax;
     decMax = _decMax;
     vMax = _vMax;
     target = _target;
+    stoppingDistance = stoppingDistanceDistr(gen);
   }
 
   double a = 0;
   double v = 0;
   double x = 0;
 
-  State st = CON;
+  State st = CON; // Initial state
 
 
   void changeState(){
     double xToTarget = target - x - stoppingDistance;
 
-    bool cond1 = v >= vMax;                                   // is at max velocity
-    // synthesis: v - vMax > 0
-    bool cond2 = xToTarget < (-1.0/2.0) * v * (v / decMax);   // needs to decelerate or else it will pass target
-    // synthesis: (oneHalf * (v * (v / decMax))) + (target - x) < stoppingDistanceMean
-    bool cond3 = v <= epsilon;                                // is at min velocity
-    // synthesis: v < 0
+    bool cond1 = v - vMax >= 0;                                   // is at max velocity
+    bool cond2 = xToTarget + v * v / (2 * decMax) < 0;            // needs to decelerate or else it will pass target
+    bool cond3 = v <= 0;                                          // is at min velocity
 
     if(cond2 && !cond3){
       st = DEC;
@@ -88,73 +83,90 @@ class Robot {
     a = st == DEC ? decMax :
               (st == ACC ? accMax : 0);
     v = vPrev + a * T_STEP;
-    bool vIsZero = v <= epsilon;
+    bool vIsZero = v <= 0;
     v = vIsZero ? 0 : v+vErr;
     x = xPrev + (v + vPrev)/2 * T_STEP;
   }
 
+  void reset(){
+    a = 0;
+    v = 0;
+    x = 0;
+    st = CON;
+  }
+
 };
 
-void writeJson(){
-  
+void writeJson(ofstream& jsonFile, bool isFirst, Robot& r){
+  string prevStateStr = r.st == 0 ? "ACC" : 
+                        (r.st == 1 ? "DEC" : "CON");
+  r.changeState();
+  r.updatePhysics();
+
+  string curStateStr = r.st == 0 ? "ACC" : 
+                    (r.st == 1 ? "DEC" : "CON");
+  if(!isFirst) jsonFile << ",";
+  jsonFile << R"({"x":{"dim":[1,0,0],"type":"NUM","name":"x","value":)";
+  jsonFile << r.x;
+  jsonFile << R"(},"v":{"dim":[1,-1,0],"type":"NUM","name":"v","value":)";
+  jsonFile << r.v;
+  jsonFile << R"(},"target":{"dim":[1,0,0],"type":"NUM","name":"target","value":)";
+  jsonFile << r.target;
+  jsonFile << R"(},"zeroVel":{"dim":[1,-1,0],"type":"NUM","name":"zeroVel","value":)";
+  jsonFile << 0;
+  jsonFile << R"(},"vMax":{"dim":[1,-1,0],"type":"NUM","name":"vMax","value":)";
+  jsonFile << r.vMax;
+  jsonFile << R"(},"decMax":{"dim":[1,-2,0],"type":"NUM","name":"decMax","value":)";
+  jsonFile << r.decMax;
+  jsonFile << R"(},"start":{"dim":[0,0,0],"type":"STATE","name":"output","value":")";
+  jsonFile << prevStateStr;
+  jsonFile << R"("},"output":{"dim":[0,0,0],"type":"STATE","name":"output","value":")";
+  jsonFile << curStateStr;
+  jsonFile << R"("}})" << endl;
 }
 
 
 
 int main() {
   
+  Robot r1 = Robot(4, -4, 15, 100);
+  Robot r2 = Robot(2, -2, 15, 100);
+  
+  if(genJson){
+    cout << "generating json\n";
+    ofstream jsonFile;
+    jsonFile.open("data.json");
+    jsonFile << "[";
+    for(double t=0; t<T_TOT/T_STEP; t++){
+      writeJson(jsonFile, t==0, r1);
+    }
+    for(double t=0; t<T_TOT/T_STEP; t++){
+      writeJson(jsonFile, false, r2);
+    }
+    jsonFile << "]";
+    jsonFile.close();
+  }
 
+  r1.reset();
+  r2.reset();
 
   if(genCsv){
     cout << "generating csv\n";
-    Robot r = Robot();
     ofstream csvFile;
     csvFile.open("data.csv");
     csvFile << "time, x, v" << "\n";
     for(double t=0; t<T_TOT/T_STEP; t++){
-      r.changeState();
-      r.updatePhysics();
-      csvFile << t << ", " << r.x << ", " << r.v << "\n";
+      r1.changeState();
+      r1.updatePhysics();
+      r2.changeState();
+      r2.updatePhysics();
+      csvFile << t << ", " << r1.x << ", " << r1.v << "\n";
+      csvFile << t << ", " << r2.x << ", " << r2.v << "\n";
     }
     csvFile.close();
   }
 
 
-  if(genJson){
-    cout << "generating json\n";
-    Robot r = Robot();
-    ofstream jsonFile;
-    jsonFile.open("data.json");
-    jsonFile << "[";
-    for(double t=0; t<T_TOT/T_STEP; t++){
-      string prevStateStr = r.st == 0 ? "ACC" : 
-                        (r.st == 1 ? "DEC" : "CON");
-      r.changeState();
-      r.updatePhysics();
-      string curStateStr = r.st == 0 ? "ACC" : 
-                        (r.st == 1 ? "DEC" : "CON");
-      if(t > 0) jsonFile << ",";
-      jsonFile << R"({"x":{"dim":[1,0,0],"type":"NUM","name":"x","value":)";
-      jsonFile << r.x;
-      jsonFile << R"(},"v":{"dim":[1,-1,0],"type":"NUM","name":"v","value":)";
-      jsonFile << r.v;
-      jsonFile << R"(},"target":{"dim":[1,0,0],"type":"NUM","name":"target","value":)";
-      jsonFile << r.target;
-      jsonFile << R"(},"oneHalf":{"dim":[0,0,0],"type":"NUM","name":"oneHalf","value":)";
-      jsonFile << 0.5;
-      jsonFile << R"(},"vMax":{"dim":[1,-1,0],"type":"NUM","name":"vMax","value":)";
-      jsonFile << r.vMax;
-      jsonFile << R"(},"decMax":{"dim":[1,-2,0],"type":"NUM","name":"decMax","value":)";
-      jsonFile << r.decMax;
-      jsonFile << R"(},"start":{"dim":[0,0,0],"type":"STATE","name":"output","value":")";
-      jsonFile << prevStateStr;
-      jsonFile << R"("},"output":{"dim":[0,0,0],"type":"STATE","name":"output","value":")";
-      jsonFile << curStateStr;
-      jsonFile << R"("}})";
-    }
-    jsonFile << "]";
-    jsonFile.close();
-  }
 
   return 0;
 }
