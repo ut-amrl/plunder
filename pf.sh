6f4f4223a0191efa93d30a30cb4ed49317383211
Jimmy Xin <jxin31415@gmail.com>
2022-06-13 17:05:11 -0500

    up particle count

--- pf.sh
+++ pf.sh
@@ -7,7 +7,7 @@ mkdir -p accSim/out
 g++ -o accSim/out/gen accSim/generate.cpp -std=c++14
 # ./gen <robot test set> <model> <mean error> <error std dev> <high-level success rate>
         # <accMax> <decMax> <maxSpeed> <targetDistance> (optional; pass in iff robot test set=3)
-./accSim/out/gen 3 0 0.0 0.15 0.8 6 -5 15 100
+./accSim/out/gen 3 0 0.0 0.2 0.8 6 -5 15 100
 
 
 # generate high-level actions from observations and low-level actions
@@ -15,7 +15,7 @@ cd pf_custom
 # ./pf <accMax> <decMax> <maxSpeed> <targetDistance> <mean error> <error std dev> <high-level success rate>
         # <model> <numParticles> <resampleThreshold> <LA std dev>
 g++ -o pf pf_runner.cpp -std=c++14
-./pf 6 -5 15 100 0.0 0.15 0.8 2 1000 0.001 10.0
+./pf 6 -5 15 100 0.0 0.2 0.8 2 1000 0.001 10.0
 
 
 # python3 plotter.py <max time steps> <particles to plot>
