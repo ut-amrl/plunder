9a97d01be59b86ee328bb194df4436f54a4d9978
Jimmy Xin <jxin31415@gmail.com>
2022-09-05 12:50:38 -0500

    Working calls to optimizer from C++!

--- scipy-tests/optimizer.py
+++ scipy-tests/optimizer.py
@@ -7,16 +7,13 @@ from scipy import optimize
 import warnings
 import time
 
-# --------------- TODO ------------------------
-# run more benchmarks
-# interface with C++
-
-
 # ------- Parameters -----------------------------
+opt_method = 0
+enumerateSigns = False
+print_debug = False
+
 max_spread = 10
 bounds_extension = 0.1
-opt_method = 0
-enumerateSigns = True
 print_warnings = False
 print_padding = 30
 
@@ -26,7 +23,10 @@ print_padding = 30
 # 2: dual annealing
 # 3: DIRECT
 
-
+# ------- Variable declarations -----------------------------
+E_k = []
+y_j = []
+clauses = []
 
 # -------- objective function --------------------
 def log_loss(x):
@@ -61,11 +61,23 @@ def log_loss(x):
 def lr(l): # list range
     return max(l)-min(l)
 
+def debug(str):
+    if print_debug:
+        print(str)
+
 def print_with_padding(label, value):
-    print((label+" ").ljust(print_padding, "-")+" "+str(value))
+    debug((label+" ").ljust(print_padding, "-")+" "+str(value))
 
 # --------- optimizer -----------------------------
-def run_optimizer(E_k, y_j, clauses):
+def run_optimizer(E_k_loc, y_j_loc, clauses_loc):
+    global E_k, y_j, clauses
+    E_k = E_k_loc
+    y_j = y_j_loc
+    clauses = clauses_loc
+
+    if(not print_warnings):
+        warnings.filterwarnings('ignore')
+
     minimizer_kwargs = {"method": "BFGS"}
     rng = np.random.default_rng()
 
@@ -76,7 +88,7 @@ def run_optimizer(E_k, y_j, clauses):
     bounds_lower = [b[0] for b in bounds]
     bounds_upper = [b[1] for b in bounds]
     print_with_padding("Bounds", "|")
-    print(bounds)
+    debug(bounds)
 
     class Bounds:
         def __init__(self, xmax=bounds_upper, xmin=bounds_lower):
@@ -108,9 +120,9 @@ def run_optimizer(E_k, y_j, clauses):
 
     # ---------- Callback ------------------------
     def print_fun(x, f, accepted):
-        # print("at minimum %.4f accepted %d" % (f, int(accepted)))
-        # print("with parameters: ")
-        # print(x)
+        # debug("at minimum %.4f accepted %d" % (f, int(accepted)))
+        # debug("with parameters: ")
+        # debug(x)
         return
 
     bestRes = -1
@@ -127,7 +139,7 @@ def run_optimizer(E_k, y_j, clauses):
         e_init = [sum(expression)/len(expression) for expression in E_k]
         init = np.concatenate((x_0_init, e_init))
         print_with_padding("Initial values", "|")
-        print(init)
+        debug(init)
 
         # -------- Optimization ----------------------
 
@@ -151,10 +163,10 @@ def run_optimizer(E_k, y_j, clauses):
             sys.exit("Please use a valid optimization method")
 
         print_with_padding("Optimal parameters", "|")
-        print(res.x)
-        # print_with_padding("Num iterations", res.nfev)
+        debug(res.x)
+        print_with_padding("Num iterations", res.nfev)
         print_with_padding("Minimum value", res.fun)
-        print()
+        debug("")
 
         if bestRes == -1 or res.fun < bestRes.fun:
             bestRes = res
@@ -163,89 +175,7 @@ def run_optimizer(E_k, y_j, clauses):
             break
     
     print_with_padding("Final parameters", "|")
-    print(bestRes.x)
+    debug(bestRes.x)
     print_with_padding("Minimum value", bestRes.fun)
-    return bestRes
 
-
-
-
-# -------- Tests, Problems, and Data -----------------------------
-
-if(not print_warnings):
-    warnings.filterwarnings('ignore')
-
-### Goal: synthesize x < 30 ----------> SUCCESS
-# https://www.desmos.com/calculator/rymyh1m1gv
-
-clauses = []
-y_j = [True, False]
-E_k = [
-    [29, 31]
-]
-
-### Tests
-assert abs(log_loss([2, 25]) - 12) < 0.001
-assert abs(log_loss([4, 35]) - 24) < 0.001
-
-# res = run_optimizer()
-# assert abs(res.x[1] - 30) < 0.05
-
-
-
-### Goal: synthesize (x > 10 && x < 20) ----------> SUCCESS
-
-clauses = [ '&' ] # (p_1 & p_2)
-y_j = [False, False, False, True, True, True, True, False, False, False, False] # whether or not each example satisfied the transition
-E_k = [ 
-    [-5, 0, 9, 11, 14, 15, 19, 21, 30, 40, 49],
-    [-5, 0, 9, 11, 14, 15, 19, 21, 30, 40, 49],
-] # value of E_k(s) for each example, for each predicate
-
-### Tests
-# res = run_optimizer()
-
-
-
-
-
-
-
-
-### Goal: synthesize (x > 10 && x < 20) || x > 50 ----------> SUCCESS (after tweaking)
-# https://www.desmos.com/calculator/1m79wd5h0e
-# https://www.desmos.com/calculator/ttucfvkvlp
-
-
-# ---------- Define examples -------------------------------------
-y_j = [False, False, False, True, True, True, True, False, False, False, False, True, True, True] # whether or not each example satisfied the transition
-E_k = [ 
-    [-5, 0, 9, 11, 14, 15, 19, 21, 30, 40, 49, 51, 55, 70],
-    [-5, 0, 9, 11, 14, 15, 19, 21, 30, 40, 49, 51, 55, 70],
-    [-5, 0, 9, 11, 14, 15, 19, 21, 30, 40, 49, 51, 55, 70],
-] # value of E_k(s) for each example, for each predicate
-
-# now introduce some error
-E_k = [ 
-    [-5, 0, 9, 11, 14, 35, 19, 21, 30, 40, 49, 51, 55, 70],
-    [-5, 0, 9, 11, 14, 35, 19, 21, 30, 40, 49, 51, 55, 70],
-    [-5, 0, 9, 11, 14, 35, 19, 21, 30, 40, 49, 51, 55, 70],
-] # value of E_k(s) for each example, for each predicate
-
-# ---------- Define equation ------------------------------------
-# clauses have left associativity?
-clauses = [ '&' , '|' ]     # (p_1 & p_2) | p_3
-
-### ----------------- Tests ---------------------------------------
-# assert abs(log_loss([2, -2, 1, 12, 18, 50]) - 4.9155) < 0.001
-# assert abs(log_loss([-1, -5, -1, 3, 18, 4]) - 176.66) < 0.001
-# assert abs(log_loss([-0.09, 0.3, 0.3, 30, 3, 53]) - 6.7191) < 0.001
-# assert abs(log_loss([3, -3, 8, 10, 20, 50]) - .195) < 0.001
-# assert abs(log_loss([ 9.17704836, -9.96464641,  9.30102843, 10.07471293, 20.03487693, 49.95211582]) - 0) < 0.001
-
-start = time.perf_counter()
-res = run_optimizer(E_k, y_j, clauses)
-end = time.perf_counter()
-
-print_with_padding("Time Elapsed", end-start)
-
+    return (bestRes.fun, list(bestRes.x))
--- scipy-tests/optimizer_runner.cpp
+++ scipy-tests/optimizer_runner.cpp
@@ -5,18 +5,59 @@
 #include <iostream>
 #include <stdexcept>
 #include <algorithm>
+#include <vector>
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {   
-    PyObject *pName, *pModule, *pFunc, *pArgs, *pValue;
+    // Test Data
+    vector<char> clauses = { '&' };
+    vector<bool> y_j = { false, false, false, true, true, true, true, false, false, false, false };
+    vector<vector<float>> E_k = {
+        {-5, 0, 9, 11, 14, 15, 19, 21, 30, 40, 49},
+        {-5, 0, 9, 11, 14, 15, 19, 21, 30, 40, 49}
+    };
 
+    vector<float> a_vals;
+    vector<float> x_0_vals;
+    float sol = 0.0;
+
+    // Initialization
     Py_Initialize();
     PyRun_SimpleString("import sys; sys.path.insert(0, './')");
 
+    PyObject *pName, *pModule, *pFunc, *pArgs, *pValue;
+    PyObject *pE_k, *pY_j, *pClauses;
+
+    // Arguments
+    pClauses = PyList_New(clauses.size());
+    for(int i = 0; i < clauses.size(); i++){
+        PyList_SetItem(pClauses, i, PyUnicode_FromString(&clauses[i]));
+    }
+    pY_j = PyList_New(y_j.size());
+    for(int i = 0; i < y_j.size(); i++){
+        PyList_SetItem(pY_j, i, (y_j[i] ? Py_True : Py_False));
+    }
+
+    pE_k = PyList_New(E_k.size());
+    for(int i = 0; i < E_k.size(); i++){
+        PyObject *subarr;
+        subarr = PyList_New(E_k[i].size());
+        for(int j = 0; j < E_k[i].size(); j++){
+            PyList_SetItem(subarr, j, PyFloat_FromDouble(E_k[i][j]));
+        }
+        PyList_SetItem(pE_k, i,  subarr);
+    }
+    pArgs = PyTuple_Pack(3, pE_k, pY_j, pClauses);
+
+    if (!pArgs) {
+        fprintf(stderr, "Cannot convert argument\n");
+        return 1;
+    }
+
     // File name
-    pName = PyUnicode_FromString((char*)"py_function");
+    pName = PyUnicode_FromString((char*)"optimizer");
 
     pModule = PyImport_Import(pName);
     Py_DECREF(pName);
@@ -24,33 +65,29 @@ int main(int argc, char *argv[])
     if(pModule != NULL) {
 
         // Function name
-        pFunc = PyObject_GetAttrString(pModule, (char*)"test");
+        pFunc = PyObject_GetAttrString(pModule, (char*)"run_optimizer");
 
         if(pFunc && PyCallable_Check(pFunc)) {
-            // Most of the function calling logic
-
-            // Arguments
-            pArgs = PyTuple_Pack(1, PyUnicode_FromString((char*)"Greg"));
-
-            if (!pValue) {
-                fprintf(stderr, "Cannot convert argument\n");
-                return 1;
-            }
-
             // Call function
             pValue = PyObject_CallObject(pFunc, pArgs);
-            Py_DECREF(pArgs);
 
-            if (pValue != NULL) {
+            if (pValue != NULL && PyTuple_Check(pValue)) {
                 // Retrieve results
-                auto result = _PyUnicode_AsString(pValue);
-                cout << result << endl;
+                sol = PyFloat_AsDouble(PyTuple_GetItem(pValue, 0));
+                pValue = PyTuple_GetItem(pValue, 1);
+                for(int i = 0; i < PyList_Size(pValue) / 2; i++){
+                    a_vals.push_back(PyFloat_AsDouble(PyList_GetItem(pValue, i)));
+                }
+                for(int i = PyList_Size(pValue) / 2; i < PyList_Size(pValue); i++){
+                    x_0_vals.push_back(PyFloat_AsDouble(PyList_GetItem(pValue, i)));
+                }
             } else {
                 PyErr_Print();
                 fprintf(stderr,"Call failed\n");
                 return 1;
             }
 
+            Py_DECREF(pArgs);
             Py_DECREF(pValue);
         } else {
             if (PyErr_Occurred())
@@ -67,6 +104,13 @@ int main(int argc, char *argv[])
     }
     
     Py_Finalize();
+
+
+    cout << "Total error: " << sol << endl;
+    for(int i = 0; i < a_vals.size(); i++){
+        cout << "logistic(" << a_vals[i] << ", " << x_0_vals[i] << "); ";
+    }
+    cout << endl;
 
     return 0;
 }
