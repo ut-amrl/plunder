85655cedb2f4e6f08d1ee1fafcf98e3fbea291f6
Jimmy Xin <jxin31415@gmail.com>
2022-12-28 22:47:31 -0600

    plotter is fruity

--- particleFilter/plotter.py
+++ particleFilter/plotter.py
@@ -1,5 +1,3 @@
-# TODO: deal with LA
-
 import matplotlib.pyplot as plt
 import numpy as np 
 import csv
@@ -13,11 +11,14 @@ trajectories = []
 gtTrajectory = []
 gtLA = []
 
+useGT = True
+
 settings = {}
 
 settings_path=""
 settings_mod_time=0
 
+# Default colors
 colors = ["#F08080", "#9FE2BF", "#6495ED", "#FFBF00", "#CCCCFF", "#40E0D0", "#DE3163", "#DFFF00", "#34495E", "#FF7F50"]
 
 # ----- I/O ---------------------------------------------=
@@ -39,6 +40,8 @@ def readTrajectories(inPath):
     return
 
 def readGroundTruth(gtPath):
+    global useGT
+
     while not os.path.exists(gtPath):
         time.sleep(1)
         # if not os.path.exists(settings_path) or os.path.getmtime(settings_path)!=settings_mod_time:
@@ -46,15 +49,39 @@ def readGroundTruth(gtPath):
     gtReader = csv.reader(open(gtPath, "r"))
     title_row = next(gtReader)
 
+    ha_index = -1
     for i in range(0, len(title_row)):
         if title_row[i].strip() == "HA":
             ha_index = i
 
+    if ha_index == -1:
+        useGT = False
+        return
+    
     for info in gtReader:
-        gtLA.append(float(info[3]))
         gtTrajectory.append(int(info[ha_index].strip()))
 
     return
+
+def readLA(gtPath):
+    global gtLA
+
+    gtReader = csv.reader(open(gtPath, "r"))
+    title_row = next(gtReader)
+
+    la_indices = []
+    la_names = []
+    for i in range(0, len(title_row)):
+        if (title_row[i].strip())[0:3] == "LA.":
+            la_indices.append(i)
+            la_names.append((title_row[i].strip())[3:])
+
+    gtLA = [[] * len(la_indices)]
+    for info in gtReader:
+        for i in range(0, len(la_indices)):
+            gtLA[i].append(float(info[la_indices[i]]))
+
+    return la_names # Return names of low-level actions
 
 def readSettings(sgPath):
     with open(sgPath) as f:
@@ -68,36 +95,34 @@ def readSettings(sgPath):
     return
 
 
-# ----- Simulation ---------------------------------------------
+# ----- Handles the main plotting logic ---------------------------------------------
 def figureHandler(outP, actions, gt, color_graph, title, iter, robot, useGT):
-    global trajectories, gtTrajectory, gtLA
+    global trajectories, gtTrajectory
     outPath = outP + str(iter) + "-" + str(robot) + "-"
 
-    maxTime = min(int(settings["PLOT_TIME"]), min(len(gtTrajectory), len(trajectories[0])))
+    maxTime = min(int(settings["PLOT_TIME"]), len(trajectories[0]))
+    if useGT:
+        maxTime = min(maxTime, len(gtTrajectory))
     PARTICLES_PLOTTED = min(int(settings["PARTICLES_PLOTTED"]), len(trajectories))
 
     for traj in trajectories:
         traj = traj[0:maxTime]
     gtTrajectory = gtTrajectory[0:maxTime]
-    gtLA = gtLA[0:maxTime]
 
     times = []
     for t in range(0, maxTime):
         times.append(t)
 
-    # Low-level actions
     if useGT:
-        fig, (ax1, ax1b, ax2, ax3) = plt.subplots(4, gridspec_kw={'height_ratios': [4, 4, 1, 1]})
+        fig, (ax1, ax1b, ax2) = plt.subplots(3, gridspec_kw={'height_ratios': [4, 4, 1]})
         ax1.margins(0)
         ax1b.margins(0)
         ax2.margins(0)
-        ax3.margins(0)
     else:
         fig, (ax1, ax1b) = plt.subplots(2, gridspec_kw={'height_ratios': [1, 1]})
         ax1.margins(0)
         ax1b.margins(0)
     
-
     fig.suptitle(title)
 
     actions = np.multiply(np.nan_to_num(np.divide(actions, np.sum(actions, axis=0))), 100)
@@ -111,15 +136,12 @@ def figureHandler(outP, actions, gt, color_graph, title, iter, robot, useGT):
 
     plt.xlabel('time')
     ax1.set_ylabel('hi-level actions')
+    ax1b.set_ylabel('hi-level actions\n(unsorted)')
 
     if useGT:
-        ax2.bar(times, gtLA, color="red", width=1)
-
         for i in range(0, len(gt)):
-            ax3.bar(times, gt[i], color=colors[i], width=1)
-
+            ax2.bar(times, gt[i], color=colors[i], width=1)
         ax2.set_ylabel('demo')
-        ax3.set_ylabel('ground\ntruth')
 
     fig.tight_layout()  
     plt.show()
@@ -133,27 +155,31 @@ def figureHandler(outP, actions, gt, color_graph, title, iter, robot, useGT):
 
     return (actions, color_graph)
 
+# Processes input and passes to figureHandler
 def plotSingle(inF, outP, gtF, title, iter, robot):
-    global trajectories, gtTrajectory, gtLA
+    global trajectories, gtTrajectory
 
     inFile = inF + str(iter) + "-" + str(robot) + ".csv"
     gtFile = gtF + str(robot) + ".csv"
 
     trajectories = []
     gtTrajectory = []
-    gtLA = []
 
     readTrajectories(inFile)
     readGroundTruth(gtFile)
 
-    maxTime = min(int(settings["PLOT_TIME"]), min(len(gtTrajectory), len(trajectories[0])))
+    maxTime = min(int(settings["PLOT_TIME"]), len(trajectories[0]))
+    if useGT:
+        maxTime = min(maxTime, len(gtTrajectory))
+
     PARTICLES_PLOTTED = min(int(settings["PARTICLES_PLOTTED"]), len(trajectories))
 
     max_action = 0
     for t in range(0, maxTime):
         for i in range(0, len(trajectories)):
             max_action = max(max_action, trajectories[i][t])
-        max_action = max(max_action, gtTrajectory[t])
+        if useGT:
+            max_action = max(max_action, gtTrajectory[t])
     
     actions = []
     gt = []
@@ -165,8 +191,9 @@ def plotSingle(inF, outP, gtF, title, iter, robot):
         for i in range(0, len(trajectories)):
             a = trajectories[i][t]
             actions[a][t] += 1
-        a = gtTrajectory[t]
-        gt[a][t] += 1
+        if useGT:
+            a = gtTrajectory[t]
+            gt[a][t] += 1
     
     color_graph = []
     for i in range(PARTICLES_PLOTTED):
@@ -175,14 +202,52 @@ def plotSingle(inF, outP, gtF, title, iter, robot):
             one_row.append(trajectories[i][j])
         color_graph.append(one_row)
 
-    return figureHandler(outP, actions, gt, color_graph, title, iter, robot, True)
+    return figureHandler(outP, actions, gt, color_graph, title, iter, robot, useGT)
 
 
 def plotSingleTimestep(inF, outP, gtF, title, iter):
     for robot in range(0, int(settings["NUM_ROBOTS"])):
-        tup = plotSingle(inF, outP, gtF, title, iter, robot)
+        plotSingle(inF, outP, gtF, title, iter, robot)
 
+# Plots low-level actions
+def plotLA(outP, gtF, robot):
+    global gtLA
 
+    gtFile = gtF + str(robot) + ".csv"
+    la_names = readLA(gtFile)
+    outPath = outP + "LA-" + str(robot) + "-"
+
+    maxTime = min(int(settings["PLOT_TIME"]), len(gtLA[0]))
+    for arr in gtLA:
+        del arr[maxTime:]
+
+    times = []
+    for t in range(0, maxTime):
+        times.append(t)
+
+    fig, axs = plt.subplots(len(gtLA))
+    axs.margins(0)
+    fig.suptitle("Low-level actions")
+    plt.xlabel('time')
+
+    # axs has a different type depending on the number of LAs
+    if len(gtLA) == 1:
+        axs.bar(times, gtLA[0], color=colors[0], width=1)
+        axs.set_ylabel(la_names[0])
+    else:
+        for i in range(0, len(gtLA)):
+            axs[i].bar(times, gtLA[i], color=colors[i], width=1)
+            axs[i].set_ylabel(la_names[i])
+
+    fig.tight_layout()  
+    plt.show()
+    if os.path.exists(outPath[:outPath.rfind('/')]):
+        plt.savefig(outPath + "graph.png")
+    else:
+        raise Exception('Plots folder deleted... restarting plotter')
+
+    plt.clf()
+    plt.close('all')
     
 # ----- Main ---------------------------------------------
 
@@ -202,12 +267,14 @@ def main():
     pureOutPath = settings["PLOT_PATH"]+"pure/"
     pfOutPath = settings["PLOT_PATH"]+"pf/"
 
-    # Plot ground truth
-
     try:
         print("Plotting ground truth...")
         for robot in range(0, int(settings["NUM_ROBOTS"])):
-            tup = plotSingle(pureInFile, pureOutPath, gtFile, 'Ground Truth Robots', 'gt', robot)
+            plotSingle(pureInFile, pureOutPath, gtFile, 'Ground Truth Robots', 'gt', robot)
+
+        print("Plotting low-level actions...")
+        for robot in range(0, int(settings["NUM_ROBOTS"])):
+            plotLA(pureOutPath, gtFile, robot)
 
         graph1 =    {   'inF': pfInFile,
                         'outP': pfOutPath,
@@ -219,6 +286,7 @@ def main():
                         'gtF': gtFile,
                         'title': 'ASP Test Run'
                     }
+            
 
         for iter in range(0, int(settings["NUM_ITER"])):
             print("Plotting particle filter graphs, iteration " + str(iter))
