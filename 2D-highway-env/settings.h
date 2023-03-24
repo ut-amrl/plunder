85e1d1be6b0a2c7c20b839d750c00d6118cdadc3
Jimmy Xin <jxin31415@gmail.com>
2023-03-23 23:19:21 -0500

    Add example snapshot for merge setting

--- 2D-highway-env/settings.h
+++ 2D-highway-env/settings.h
@@ -6,28 +6,30 @@ using namespace std;
 
 namespace SETTINGS {
 
-    // I/O parameters
-    const string SIM_DATA = "python-gen/data";                         // Generated data from simulation, contains HA, LA, and observed state sequences
+    // ----- I/O parameters -----
+    const string SIM_DATA = "python-gen/data";          // Generated data from simulation, contains HA, LA, and observed state sequences
+                                                        // Default: sim/data, is where our provided generator will store these files
     
-    // General Configuration
+    // ----- General configuration -----
     const bool DEBUG = false;
-    const int TRAINING_SET = 10;
-    const int VALIDATION_SET = 30;
-    const bool USE_SAFE_TRANSITIONS = false;          // "safe" transitions (only allow user-specified transitions)
+    const int TRAINING_SET = 10;                // Number of robots to train on
+    const int VALIDATION_SET = 30;              // Number of total robots in the full set (including the training set)
+    const bool USE_SAFE_TRANSITIONS = false;    // "Safe" transitions (only allow user-specified transitions). 
+                                                // Optional modification to improve performance on policies with sparse transitions
+    // ----- Simulation parameters -----
+    const double T_STEP = -1;                   // Time step (s)
+    const double T_TOT = -1;                    // Total time (s) per simulated scenario
+    const double GEN_ACCURACY = -1;             // Probability of a correct high-level transition in the simulation
 
-    // Simulation parameters
-    const double T_STEP = -1;               // time step (s)
-    const double T_TOT = -1;                // total time (s) per simulated scenario
-    const double GEN_ACCURACY = -1;        // probability of a correct high-level transition in the simulation
+    // ----- EM Loop parameters -----
+    const int NUM_ITER = 20;                    // Number of iterations in the expectation-maximization loop
 
-    // EM Loop parameters
-    const int NUM_ITER = 20;                    // number of iterations in the expectation-maximization loop
+    // ----- Parallelization Parameters -----
+    const int BATCH_SIZE = 16;                  // Number of programs to optimize in parallel
+    const int NUM_CORES = 8;                    // Number of cores to use per program: NUM_CORES * BATCH_SIZE = total number of cores used at once
 
-    // Optimization parameters
-    const int BATCH_SIZE = 16;               // Number of programs to optimize in parallel
-    const int NUM_CORES = 8;                // Number of cores to use per program: NUM_CORES * BATCH_SIZE = total number of cores used at once
-
+    // ----- Important hyperparameters -----
     const double PROG_COMPLEXITY_LOSS_BASE = 0.003;     // adds L1 loss ( AST.size * PROG_COMPLEXITY_LOSS )
-    const double PROG_COMPLEXITY_LOSS = 0.085;           // adds L1 loss ( AST.size * PROG_COMPLEXITY_LOSS * loss)
-    const double ALPHA_LOSS_UPPER = 0.0001;           // adds L2 loss ( alpha^2 * ALPHA_LOSS_UPPER )                        // TEMPERATURE decreases linearly by this much each iteration
+    const double PROG_COMPLEXITY_LOSS = 0.085;          // adds L1 loss ( AST.size * PROG_COMPLEXITY_LOSS * loss )
+    const double ALPHA_LOSS_UPPER = 0.0001;             // adds L2 loss ( alpha^2 * ALPHA_LOSS_UPPER )
 }
