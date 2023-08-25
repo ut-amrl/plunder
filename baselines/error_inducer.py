94fa1408c5d12ce886f62bbfee684c55b1ba7720
Jimmy Xin <jxin31415@gmail.com>
2023-08-25 13:17:22 -0500

    Add error inducer

---
+++ baselines/error_inducer.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import math
+
+in_files = "supervised_learning_la/merge-easy-data/"
+out_files = "supervised_learning_la/merge-medium-data/"
+out_files2 = "supervised_learning_ha/merge-medium-data/"
+
+num_traj = 30
+
+pv1_dev = 0.005
+pv2_dev = 0.5
+
+target_pv1_dev = 0.01
+target_pv2_dev = 1
+
+add_pv1_dev = math.sqrt(target_pv1_dev * target_pv1_dev - pv1_dev * pv1_dev)
+add_pv2_dev = math.sqrt(target_pv2_dev * target_pv2_dev - pv2_dev * pv2_dev)
+
+print(add_pv1_dev)
+print(add_pv2_dev)
+
+pv1 = "LA.steer"
+pv2 = "LA.acc"
+
+for traj in range(0, num_traj):
+    df = pd.read_csv(in_files + "data" + str(traj) + ".csv", skipinitialspace=True)
+
+    df[pv1] = df.apply(lambda row: row[pv1] + np.random.normal(0, add_pv1_dev), axis=1)
+    df[pv2] = df.apply(lambda row: row[pv2] + np.random.normal(0, add_pv2_dev), axis=1)
+    
+    np.savetxt(out_files + "data" + str(traj) + ".csv", df, delimiter=", ", header=", ".join(df.columns.values), fmt='%s', comments='', encoding=None)
+    np.savetxt(out_files2 + "data" + str(traj) + ".csv", df, delimiter=", ", header=", ".join(df.columns.values), fmt='%s', comments='', encoding=None)
--- baselines/supervised_learning_la/settings.py
+++ baselines/supervised_learning_la/settings.py
@@ -2,40 +2,40 @@ import pandas as pd
 import numpy as np
 
 # Setting: 1D-target
-training_set = 10
-validation_set = 30 # including training_set
-train_time = 10000
-patience = 500
-sim_time = 126
-samples = 100
-folder = "target/"
-vars_used = [
-    "HA",
-    "pos",
-    "decMax",
-    "accMax",
-    "vel",
-    "vMax",
-    "LA.acc",
-]
-pred_var1 = "LA.acc"
-pv1_range = [-50, 50]
-pv1_stddev = 0.5
-pred_var2 = None
-pv2_range = None
-pv2_stddev = None
+# training_set = 10
+# validation_set = 30 # including training_set
+# train_time = 10000
+# patience = 500
+# sim_time = 126
+# samples = 100
+# folder = "target/"
+# vars_used = [
+#     "HA",
+#     "pos",
+#     "decMax",
+#     "accMax",
+#     "vel",
+#     "vMax",
+#     "LA.acc",
+# ]
+# pred_var1 = "LA.acc"
+# pv1_range = [-50, 50]
+# pv1_stddev = 0.5
+# pred_var2 = None
+# pv2_range = None
+# pv2_stddev = None
 
-numHA = 3
-def motor_model(ha, data, data_prev):
-    if ha == 0:
-        return (min(data_prev["LA.acc"] + 1, data["accMax"]), None)
-    elif ha == 1:
-        if data_prev["LA.acc"] < 0:
-            return (min(data_prev["LA.acc"] + 1, data["accMax"]), None)
-        elif data_prev["LA.acc"] > 0:
-            return (max(data_prev["LA.acc"] - 1, data["decMax"]), None)
-    else:
-        return (max(data_prev["LA.acc"] - 1, data["decMax"]), None)
+# numHA = 3
+# def motor_model(ha, data, data_prev):
+#     if ha == 0:
+#         return (min(data_prev["LA.acc"] + 1, data["accMax"]), None)
+#     elif ha == 1:
+#         if data_prev["LA.acc"] < 0:
+#             return (min(data_prev["LA.acc"] + 1, data["accMax"]), None)
+#         elif data_prev["LA.acc"] > 0:
+#             return (max(data_prev["LA.acc"] - 1, data["decMax"]), None)
+#     else:
+#         return (max(data_prev["LA.acc"] - 1, data["decMax"]), None)
 
 # Setting: 2D-highway-env
 # training_set = 10
@@ -112,74 +112,74 @@ def motor_model(ha, data, data_prev):
 
 
 # Setting: 2D-merge
-# training_set = 10
-# validation_set = 30 # including training_set
-# train_time = 15000
-# patience = 500
-# sim_time = 75
-# samples = 50
-# # folder = "merge-easy-data/"
+training_set = 10
+validation_set = 30 # including training_set
+train_time = 15000
+patience = 500
+sim_time = 75
+samples = 50
+folder = "merge-easy-data/"
 # folder = "merge-medium-data/"
-# vars_used = [
-#     "HA",
-#     "x",
-#     "l_x",
-#     "f_x",
-#     "r_x",
-#     "v_x",
-#     "l_vx",
-#     "f_vx",
-#     "r_vx",
-#     "LA.steer",
-#     "LA.acc"
-# ]
-# pred_var1 = "LA.steer"
-# pv1_range = [-0.3, 0.3]
-# pv1_stddev = 0.01
-# pred_var2 = "LA.acc"
-# pv2_range = [-30, 30]
-# pv2_stddev = 1
+vars_used = [
+    "HA",
+    "x",
+    "l_x",
+    "f_x",
+    "r_x",
+    "v_x",
+    "l_vx",
+    "f_vx",
+    "r_vx",
+    "LA.steer",
+    "LA.acc"
+]
+pred_var1 = "LA.steer"
+pv1_range = [-0.3, 0.3]
+pv1_stddev = 0.03
+pred_var2 = "LA.acc"
+pv2_range = [-30, 30]
+pv2_stddev = 3
 
-# numHA = 4
+numHA = 4
 
-# TURN_HEADING = 0.15 # Target heading when turning
-# TURN_TARGET = 30 # How much to adjust when targeting a lane (higher = smoother)
-# max_velocity = 45 # Maximum velocity
+TURN_HEADING = 0.15 # Target heading when turning
+TURN_TARGET = 30 # How much to adjust when targeting a lane (higher = smoother)
+max_velocity = 45 # Maximum velocity
 
-# def laneFinder(y):
-#     return round(y / 4)
+def laneFinder(y):
+    return round(y / 4)
 
-# def motor_model(ha, data, data_prev):
-#     target_acc = 0.0
-#     target_heading = 0.0
+def motor_model(ha, data, data_prev):
+    target_acc = 0.0
+    target_heading = 0.0
 
-#     if ha == 0:
-#         target_acc = max_velocity - data["vx"]
+    if ha == 0:
+        target_acc = max_velocity - data["vx"]
 
-#         target_y = laneFinder(data["y"]) * 4
-#         target_heading = np.arctan((target_y - data["y"]) / TURN_TARGET)
-#     elif ha == 1:
-#         target_acc = data["f_vx"] - data["vx"]
+        target_y = laneFinder(data["y"]) * 4
+        target_heading = np.arctan((target_y - data["y"]) / TURN_TARGET)
+    elif ha == 1:
+        target_acc = data["f_vx"] - data["vx"]
 
-#         target_y = laneFinder(data["y"]) * 4
-#         target_heading = np.arctan((target_y - data["y"]) / TURN_TARGET)
-#     elif ha == 2:
-#         target_acc = -0.5
-#         target_heading = -TURN_HEADING
-#     else:
-#         target_acc = -0.5
-#         target_heading = TURN_HEADING
+        target_y = laneFinder(data["y"]) * 4
+        target_heading = np.arctan((target_y - data["y"]) / TURN_TARGET)
+    elif ha == 2:
+        target_acc = -0.5
+        target_heading = -TURN_HEADING
+    else:
+        target_acc = -0.5
+        target_heading = TURN_HEADING
 
-#     target_steer = target_heading - data["heading"]
+    target_steer = target_heading - data["heading"]
 
-#     if target_steer > data_prev["LA.steer"]:
-#         target_steer = min(target_steer, data_prev["LA.steer"] + 0.08)
-#     else:
-#         target_steer = max(target_steer, data_prev["LA.steer"] - 0.08)
+    if target_steer > data_prev["LA.steer"]:
+        target_steer = min(target_steer, data_prev["LA.steer"] + 0.08)
+    else:
+        target_steer = max(target_steer, data_prev["LA.steer"] - 0.08)
 
-#     if target_acc > data_prev["LA.acc"]:
-#         target_acc = min(target_acc, data_prev["LA.acc"] + 4)
-#     else:
-#         target_acc = max(target_acc, data_prev["LA.acc"] - 6)
+    if target_acc > data_prev["LA.acc"]:
+        target_acc = min(target_acc, data_prev["LA.acc"] + 4)
+    else:
+        target_acc = max(target_acc, data_prev["LA.acc"] - 6)
 
-#     return (target_steer, target_acc)
+    return (target_steer, target_acc)
