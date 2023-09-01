cd624bfcdd998b2316453143c35b750037fbcabd
Jimmy Xin <jxin31415@gmail.com>
2023-08-31 23:33:55 -0500

    Train DDPG on pick-place

---
+++ panda-pick-place/python-gen/ddpg.py
@@ -0,0 +1,48 @@
+import gymnasium as gym
+import panda_gym
+from stable_baselines3 import DDPG
+import time
+
+env = gym.make("PandaPickAndPlace-v3")
+model = DDPG(policy="MultiInputPolicy", env=env)
+model.learn(30_000, progress_bar=True)
+
+model.save("ddpg-pick-and-place")
+
+env = gym.make("PandaPickAndPlace-v3", render_mode="human")
+for iter in range(30):
+    obs_out = open("data" + str(iter) + ".csv", "w")
+    obs_out.write("x, y, z, bx, by, bz, tx, ty, tz, end_width, LA.vx, LA.vy, LA.vz, LA.end, HA\n")
+
+    observation, info = env.reset()
+
+    action = [0, 0, 0, 0]
+
+    for _ in range(50):
+        observation, reward, terminated, truncated, info = env.step(action)
+        action, _states = model.predict(observation, deterministic=True)
+        
+        world_state = observation["observation"]
+        target_pos = observation["desired_goal"][0:3]
+
+        x, y, z, bx, by, bz, tx, ty, tz, end_width = world_state[0], world_state[1], world_state[2], world_state[7], world_state[8], world_state[9], target_pos[0], target_pos[1], target_pos[2], world_state[6]
+        bx, by, bz = bx - x, by - y, bz - z
+        tx, ty, tz = tx - x, ty - y, tz - z
+
+        obs_pruned = [x, y, z, bx, by, bz, tx, ty, tz, end_width]
+
+        for each in obs_pruned:
+            obs_out.write(str(each)+", ")
+        for each in action:
+            obs_out.write(str(each)+", ")
+
+        obs_out.write("0\n")
+        
+        time.sleep(0.1)
+        if terminated or truncated:
+            break
+    
+    obs_out.close()
+    
+
+env.close()
