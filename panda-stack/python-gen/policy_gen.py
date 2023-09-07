7ba0f34035f98e3c1b9fca96c35627b0a86b902d
Jimmy Xin <jxin31415@gmail.com>
2023-09-06 23:36:38 -0500

    Add generators for stack env

--- panda-stack/python-gen/policy_gen.py
+++ panda-stack/python-gen/policy_gen.py
@@ -4,64 +4,92 @@ import time
 import numpy as np
 
 def asp(observation, ha) -> str:
-    x, y, z, bx, by, bz, tx, ty, tz, end_width = observation[0], observation[1], observation[2], observation[3], observation[4], observation[5], observation[6], observation[7], observation[8], observation[9]
-    if ha == 'MOVE_TO_CUBE' and abs(bx) < 0.01 and abs(by) < 0.015 and abs(bz) < 0.01:
+    x, y, z, end_width = observation[0], observation[1], observation[2], observation[3]
+    bx1, by1, bz1, bx2, by2, bz2 = observation[4], observation[5], observation[6], observation[7], observation[8], observation[9]
+    tx1, ty1, tz1, tx2, ty2, tz2 = observation[10], observation[11], observation[12], observation[13], observation[14], observation[15]
+
+    bx1, by1, bz1, bx2, by2, bz2 = bx1 - x, by1 - y, bz1 - z, bx2 - x, by2 - y, bz2 - z
+    tx1, ty1, tz1, tx2, ty2, tz2 = tx1 - x, ty1 - y, tz1 - z, tx2 - x, ty2 - y, tz2 - z
+
+    if ha == 'MOVE_TO_CUBE_BOTTOM' and abs(bx1) < 0.002 and abs(by1) < 0.002 and abs(bz1) < 0.005:
+        return 'MOVE_TO_TARGET'
+    elif ha == 'MOVE_TO_TARGET' and abs(tx1) < 0.002 and abs(ty1) < 0.002:
+        return 'LIFT'
+    elif ha == 'LIFT' and z > 0.2 and observation[9] < 0.03:
+        return 'MOVE_TO_CUBE_TOP'
+    elif ha == 'MOVE_TO_CUBE_TOP' and abs(bx2) < 0.003 and abs(by2) < 0.003 and abs(bz2) < 0.005:
         return 'GRASP'
-    elif ha == 'GRASP' and end_width < 0.04:
+    elif ha == 'GRASP' and z > 0.2:
         return 'MOVE_TO_TARGET'
     return ha
 
 def get_action(observation, ha) -> str:
-    x, y, z, bx, by, bz, tx, ty, tz, end_width = observation[0], observation[1], observation[2], observation[3], observation[4], observation[5], observation[6], observation[7], observation[8], observation[9]
-    if ha == 'MOVE_TO_CUBE':
-        return [bx * 5.0, by * 5.0, bz * 5.0, 1]
+    x, y, z, end_width = observation[0], observation[1], observation[2], observation[3]
+    bx1, by1, bz1, bx2, by2, bz2 = observation[4], observation[5], observation[6], observation[7], observation[8], observation[9]
+    tx1, ty1, tz1, tx2, ty2, tz2 = observation[10], observation[11], observation[12], observation[13], observation[14], observation[15]
+
+    bx1, by1, bz1, bx2, by2, bz2 = bx1 - x, by1 - y, bz1 - z, bx2 - x, by2 - y, bz2 - z
+    tx1, ty1, tz1, tx2, ty2, tz2 = tx1 - x, ty1 - y, tz1 - z, tx2 - x, ty2 - y, tz2 - z
+
+    if ha == 'MOVE_TO_CUBE_BOTTOM':
+        return [bx1 * 5.0, by1 * 5.0, bz1 * 5.0, 1]
+    elif ha == 'MOVE_TO_TARGET':
+        return [tx2 * 5.0, ty2 * 5.0, tz2 * 5.0, -1]
+    elif ha == 'LIFT':
+        return [0, 0, 0.5, 1]
+    elif ha == 'MOVE_TO_CUBE_TOP':
+        return [bx2 * 5.0, by2 * 5.0, bz2 * 5.0, 1]
     elif ha == 'GRASP':
-        return [0, 0, 0, -1]
-    else:
-        return [tx * 5.0, ty * 5.0, tz * 5.0, -1]
+        return [0, 0, 0.5, -1]
 
-env = gym.make("PandaPickAndPlace-v3", render_mode="human")
+env = gym.make("PandaStack-v3", render_mode="human")
 
-for iter in range(30):
+for iter in range(15):
     obs_out = open("data" + str(iter) + ".csv", "w")
-    obs_out.write("x, y, z, bx, by, bz, tx, ty, tz, end_width, LA.vx, LA.vy, LA.vz, LA.end, HA\n")
+    obs_out.write("x, y, z, end_width, bx1, by1, bz1, bx2, by2, bz2, tx1, ty1, tz1, tx2, ty2, tz2, LA.vx, LA.vy, LA.vz, LA.end, HA\n")
 
     observation, info = env.reset()
 
-    ha = 'MOVE_TO_CUBE'
+    ha = 'MOVE_TO_CUBE_BOTTOM'
     action = [0, 0, 0, 0]
 
-    for _ in range(30):
+    for _ in range(150):
         observation, reward, terminated, truncated, info = env.step(action)
 
         world_state = observation["observation"]
-        target_pos = observation["desired_goal"][0:3]
+        target_bottom = observation["desired_goal"][0:3]
+        target_top = observation["desired_goal"][3:6]
 
-        x, y, z, bx, by, bz, tx, ty, tz, end_width = world_state[0], world_state[1], world_state[2], world_state[7], world_state[8], world_state[9], target_pos[0], target_pos[1], target_pos[2], world_state[6]
-        bx, by, bz = bx - x, by - y, bz - z
-        tx, ty, tz = tx - x, ty - y, tz - z
+        x, y, z, end_width = world_state[0], world_state[1], world_state[2], world_state[6]
+        bx1, by1, bz1, bx2, by2, bz2 = world_state[7], world_state[8], world_state[9], world_state[19], world_state[20], world_state[21]
+        tx1, ty1, tz1, tx2, ty2, tz2 = target_bottom[0], target_bottom[1], target_bottom[2], target_top[0], target_top[1], target_top[2]
 
-        obs_pruned = [x, y, z, bx, by, bz, tx, ty, tz, end_width]
+        obs_pruned = [x, y, z, end_width, bx1, by1, bz1, bx2, by2, bz2, tx1, ty1, tz1, tx2, ty2, tz2]
         ha = asp(obs_pruned, ha)
         action = get_action(obs_pruned, ha)
-
-        for each in obs_pruned:
-            obs_out.write(str(each)+", ")
-        for each in action:
-            obs_out.write(str(each)+", ")
-
-        if ha == 'MOVE_TO_CUBE':
-            obs_out.write("0\n")
-        elif ha == 'GRASP':
-            obs_out.write("1\n")
-        elif ha == 'MOVE_TO_TARGET':
-            obs_out.write("2\n")
         
-        # time.sleep(0.1)
+        for each in obs_pruned:
+            with_err = np.random.normal(each, 0.0001)
+            obs_out.write(str(with_err)+", ")
+        for each in action:
+            with_err = np.random.normal(each, 0.3)
+            obs_out.write(str(with_err)+", ")
+
+        if ha == 'MOVE_TO_CUBE_BOTTOM':
+            obs_out.write("0\n")
+        elif ha == 'MOVE_TO_TARGET':
+            obs_out.write("1\n")
+        elif ha == 'LIFT':
+            obs_out.write("2")
+        elif ha == 'MOVE_TO_CUBE_TOP':
+            obs_out.write("3\n")
+        elif ha == 'GRASP':
+            obs_out.write("4\n")
+        
+        time.sleep(0.05)
         # if terminated or truncated:
         #     break
     
     obs_out.close()
-    
 
 env.close()
--- panda-stack/python-gen/train_rl.py
+++ panda-stack/python-gen/train_rl.py
@@ -4,41 +4,41 @@ from stable_baselines3 import PPO, A2C, DDPG
 from sb3_contrib import TQC
 import time
 
-algo = "A2C"
+algo = "TQC"
 from_existing = True
-learn_time = 1_000
+learn_time = 1_000_000
 
-env = gym.make("PandaPickAndPlaceDense-v3")
+env = gym.make("PandaStackDense-v3")
 
 if algo == "DDPG":
     if from_existing: 
-        model = DDPG.load("ddpg-pick-and-place", env=env)
+        model = DDPG.load("ddpg-stack", env=env)
     else:
         model = DDPG(policy="MultiInputPolicy", env=env)
     model.learn(learn_time, progress_bar=True)
-    model.save("ddpg-pick-and-place")
+    model.save("ddpg-stack-new")
 
 elif algo == "A2C":
     if from_existing: 
-        model = A2C.load("a2c-pick-and-place", env=env)
+        model = A2C.load("a2c-stack", env=env)
     else:
         model = A2C(policy="MultiInputPolicy", env=env)
     model.learn(learn_time, progress_bar=True)
-    model.save("a2c-pick-and-place")
+    model.save("a2c-stack-new")
 
 elif algo == "PPO":
     if from_existing: 
-        model = PPO.load("ppo-pick-and-place", env=env)
+        model = PPO.load("ppo-stack", env=env)
     else:
         model = PPO(policy="MultiInputPolicy", env=env)
     model.learn(learn_time, progress_bar=True)
-    model.save("ppo-pick-and-place")
+    model.save("ppo-stack-new")
 
 elif algo == "TQC":
     if from_existing: 
-        model = TQC.load("tqc-pick-and-place", env=env)
+        model = TQC.load("tqc-stack", env=env)
     else:
         model = TQC(policy="MultiInputPolicy", env=env)
     model.learn(learn_time, progress_bar=True)
-    model.save("tqc-pick-and-place")
+    model.save("tqc-stack-new")
 
