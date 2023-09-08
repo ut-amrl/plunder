9bf0bb23c883de99cfbb8855215e3f185fd23ce1
Jimmy Xin <jxin31415@gmail.com>
2023-09-07 23:06:13 -0500

    panda-stack bug fix

--- panda-stack/robot.h
+++ panda-stack/robot.h
@@ -8,10 +8,10 @@ using namespace SETTINGS;
 
 // MOTOR (OBSERVATION) MODEL: known function mapping from high-level to low-level actions
 map<string, normal_distribution<double>> la_error = {
-    { "vx", normal_distribution<double>(0.0, 0.3) },
-    { "vy", normal_distribution<double>(0.0, 0.3) },
-    { "vz", normal_distribution<double>(0.0, 0.3) },
-    { "end", normal_distribution<double>(0.0, 0.3) }
+    { "vx", normal_distribution<double>(0.0, 0.5) },
+    { "vy", normal_distribution<double>(0.0, 0.5) },
+    { "vz", normal_distribution<double>(0.0, 0.5) },
+    { "end", normal_distribution<double>(0.0, 0.5) }
 };
 double la_error_scaler = 1.0;
 
@@ -53,15 +53,15 @@ Obs motorModel(State state, bool error){
 
 HA ASP_model(State state){
     HA ha = state.ha;
-    int bx1 = abs(state.get("bx1") - state.get("x")),
-        by1 = abs(state.get("by1") - state.get("y")), 
+    double bx1 = abs(state.get("bx1") - state.get("x")),
+        by1 = abs(state.get("by1") - state.get("y")),
         bz1 = abs(state.get("bz1") - state.get("z")), 
         bx2 = abs(state.get("bx2") - state.get("x")), 
         by2 = abs(state.get("by2") - state.get("y")), 
-        bz2 = abs(state.get("bz2") - state.get("z"));
-    int tx1 = abs(state.get("tx1") - state.get("x")),
-        ty1 = abs(state.get("ty1") - state.get("y")), 
-        tz1 = abs(state.get("tz1") - state.get("z"));
+        bz2 = abs(state.get("bz2") - state.get("z")),
+        tx1 = abs(state.get("tx2") - state.get("x")),
+        ty1 = abs(state.get("ty2") - state.get("y")), 
+        tz1 = abs(state.get("tz2") - state.get("z"));
 
     if(ha == MOVE_TO_CUBE_BOTTOM && bx1 < 0.002 && by1 < 0.002 && bz1 < 0.005) {
         return MOVE_TO_TARGET;
@@ -72,7 +72,7 @@ HA ASP_model(State state){
     if(ha == LIFT && state.get("z") > 0.2 && state.get("bz2") < 0.03) {
         return MOVE_TO_CUBE_TOP;
     }
-    if(ha == MOVE_TO_CUBE_TOP && bx2 < 0.002 && by2 < 0.002 && bz2 < 0.005) {
+    if(ha == MOVE_TO_CUBE_TOP && bx2 < 0.003 && by2 < 0.003 && bz2 < 0.005) {
         return GRASP;
     }
     if(ha == GRASP && state.get("z") > 0.2) {
