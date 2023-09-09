0cb3b53ac8e4e04e4773602e32849c87a128d8f7
Jimmy Xin <jxin31415@gmail.com>
2023-09-09 18:17:14 -0500

    Clean up ground truth policy

--- panda-stack/python-gen/policy_gen.py
+++ panda-stack/python-gen/policy_gen.py
@@ -2,24 +2,30 @@ import gymnasium as gym
 import panda_gym
 import time
 import numpy as np
+import random
+
+######## ASP ########
+# Probabilistic functions
+def logistic(offset, slope, x):
+    return 1.0/(1.0+np.exp(-slope*(x-offset)))
+
+def sample(p):
+    return random.random()<p
 
 def asp(observation, ha) -> str:
     x, y, z, end_width = observation[0], observation[1], observation[2], observation[3]
     bx1, by1, bz1, bx2, by2, bz2 = observation[4], observation[5], observation[6], observation[7], observation[8], observation[9]
     tx2, ty2, tz2 = observation[13], observation[14], observation[15]
 
-    bx1, by1, bz1, bx2, by2, bz2 = bx1 - x, by1 - y, bz1 - z, bx2 - x, by2 - y, bz2 - z
-    tx2, ty2, tz2 = tx2 - x, ty2 - y, tz2 - z + 0.01
-
-    if ha == 'MOVE_TO_CUBE_BOTTOM' and abs(bx1) < 0.002 and abs(by1) < 0.002 and abs(bz1) < 0.006:
+    if ha == 'MOVE_TO_CUBE_BOTTOM' and bz1 > -0.002:
         return 'MOVE_TO_TARGET'
-    elif ha == 'MOVE_TO_TARGET' and abs(tx2) < 0.002 and abs(ty2) < 0.002 and abs(tz2) < 0.002:
+    elif ha == 'MOVE_TO_TARGET' and abs(tx2) < 0.002 and abs(ty2) < 0.002:
         return 'LIFT'
-    elif ha == 'LIFT' and z > 0.1 and observation[9] < 0.03:
+    elif ha == 'LIFT' and sample(logistic(0.1, 100, z)) and bz2 - bz1 < 0.03:
         return 'MOVE_TO_CUBE_TOP'
-    elif ha == 'MOVE_TO_CUBE_TOP' and abs(bx2) < 0.002 and abs(by2) < 0.002 and abs(bz2) < 0.006:
+    elif ha == 'MOVE_TO_CUBE_TOP' and bz2 > -0.002:
         return 'GRASP'
-    elif ha == 'GRASP' and z > 0.1:
+    elif ha == 'GRASP' and sample(logistic(0.1, 100, z)):
         return 'MOVE_TO_TARGET'
     return ha
 
@@ -28,24 +34,23 @@ def get_action(observation, past_action, ha) -> str:
     bx1, by1, bz1, bx2, by2, bz2 = observation[4], observation[5], observation[6], observation[7], observation[8], observation[9]
     tx2, ty2, tz2 = observation[13], observation[14], observation[15]
 
-    bx1, by1, bz1, bx2, by2, bz2 = bx1 - x, by1 - y, bz1 - z, bx2 - x, by2 - y, bz2 - z
-    tx2, ty2, tz2 = tx2 - x, ty2 - y, tz2 - z + 0.01
+    tz2 += 0.01
 
     if ha == 'MOVE_TO_CUBE_BOTTOM':
         action = [bx1 * 4.0, by1 * 4.0, bz1 * 4.0, 1]
     elif ha == 'MOVE_TO_TARGET':
         action = [tx2 * 4.0, ty2 * 4.0, tz2 * 4.0, -1]
+        if action[2] < 0:
+            action[2] = max(past_action[2] - 0.05, action[2])
     elif ha == 'LIFT':
         action = [0, 0, 0.5, 1]
     elif ha == 'MOVE_TO_CUBE_TOP':
         action = [bx2 * 4.0, by2 * 4.0, bz2 * 4.0, 1]
+        if action[2] < 0:
+            action[2] = max(past_action[2] - 0.05, action[2])
     elif ha == 'GRASP':
         action = [0, 0, 0.5, -1]
 
-    # Make actions continuous
-    for i in range(len(action) - 1):
-        d_action = min(0.03, abs(action[i] / 4))
-        action[i] = min(max(action[i], past_action[i] - d_action), past_action[i] + d_action)
     return action
 
 env = gym.make("PandaStackDense-v3", render_mode="human")
@@ -53,7 +58,7 @@ env = gym.make("PandaStackDense-v3", render_mode="human")
 def bound(x):
     return max(min(x, 1), -1)
 
-for iter in range(15):
+for iter in range(20):
     obs_out = open("data" + str(iter) + ".csv", "w")
     obs_out.write("x, y, z, end_width, bx1, by1, bz1, bx2, by2, bz2, tx1, ty1, tz1, tx2, ty2, tz2, LA.vx, LA.vy, LA.vz, LA.end, HA\n")
 
@@ -62,7 +67,7 @@ for iter in range(15):
     ha = 'MOVE_TO_CUBE_BOTTOM'
     action = [0, 0, 0, 0]
 
-    for _ in range(250):
+    for _ in range(175):
         observation, reward, terminated, truncated, info = env.step(action)
 
         world_state = observation["observation"]
@@ -73,15 +78,17 @@ for iter in range(15):
         bx1, by1, bz1, bx2, by2, bz2 = world_state[7], world_state[8], world_state[9], world_state[19], world_state[20], world_state[21]
         tx1, ty1, tz1, tx2, ty2, tz2 = target_bottom[0], target_bottom[1], target_bottom[2], target_top[0], target_top[1], target_top[2]
 
+        bx1, by1, bz1, bx2, by2, bz2 = bx1 - x, by1 - y, bz1 - z, bx2 - x, by2 - y, bz2 - z
+        tx2, ty2, tz2 = tx2 - x, ty2 - y, tz2 - z
+
         obs_pruned = [x, y, z, end_width, bx1, by1, bz1, bx2, by2, bz2, tx1, ty1, tz1, tx2, ty2, tz2]
         ha = asp(obs_pruned, ha)
         action = get_action(obs_pruned, action, ha)
         
         for each in obs_pruned:
-            with_err = np.random.normal(each, 0.0005)
-            obs_out.write(str(with_err)+", ")
+            obs_out.write(str(each)+", ")
         for each in action:
-            with_err = bound(np.random.normal(each, 0.01))
+            with_err = bound(np.random.normal(each, 0.3))
             obs_out.write(str(with_err)+", ")
 
         if ha == 'MOVE_TO_CUBE_BOTTOM':
@@ -95,7 +102,7 @@ for iter in range(15):
         elif ha == 'GRASP':
             obs_out.write("4\n")
         
-        time.sleep(0.04)
+        time.sleep(0.05)
         # if terminated or truncated:
         #     break
     
--- panda-stack/robot.h
+++ panda-stack/robot.h
@@ -8,10 +8,10 @@ using namespace SETTINGS;
 
 // MOTOR (OBSERVATION) MODEL: known function mapping from high-level to low-level actions
 map<string, normal_distribution<double>> la_error = {
-    { "vx", normal_distribution<double>(0.0, 0.01) },
-    { "vy", normal_distribution<double>(0.0, 0.01) },
-    { "vz", normal_distribution<double>(0.0, 0.01) },
-    { "end", normal_distribution<double>(0.0, 0.01) }
+    { "vx", normal_distribution<double>(0.0, 0.3) },
+    { "vy", normal_distribution<double>(0.0, 0.3) },
+    { "vz", normal_distribution<double>(0.0, 0.3) },
+    { "end", normal_distribution<double>(0.0, 0.3) }
 };
 double la_error_scaler = 1.0;
 
@@ -22,69 +22,65 @@ double bound(double input) {
 // Custom-defined motor models
 Obs motorModel(State state, bool error){
     HA ha = state.ha;
-    double bx1 = state.get("bx1") - state.get("x"),
-        by1 = state.get("by1") - state.get("y"),
-        bz1 = state.get("bz1") - state.get("z"), 
-        bx2 = state.get("bx2") - state.get("x"), 
-        by2 = state.get("by2") - state.get("y"), 
-        bz2 = state.get("bz2") - state.get("z"),
-        tx2 = state.get("tx2") - state.get("x"),
-        ty2 = state.get("ty2") - state.get("y"), 
-        tz2 = state.get("tz2") - state.get("z") + 0.01;
+    double bx1 = state.get("bx1"),
+        by1 = state.get("by1"),
+        bz1 = state.get("bz1"),
+        bx2 = state.get("bx2"),
+        by2 = state.get("by2"),
+        bz2 = state.get("bz2"),
+        tx2 = state.get("tx2"),
+        ty2 = state.get("ty2"),
+        tz2 = state.get("tz2") + 0.01;
 
     double vx, vy, vz, end;
     if(ha == MOVE_TO_CUBE_BOTTOM) {
         vx = 4 * bx1, vy = 4 * by1, vz = 4 * bz1, end = 1;
     } else if (ha == MOVE_TO_TARGET) {
         vx = 4 * tx2, vy = 4 * ty2, vz = 4 * tz2, end = -1;
+        if (vz < 0) {
+            vz = max(state.get("vz") - 0.05, vz);
+        }
     } else if (ha == LIFT) {
         vx = 0, vy = 0, vz = 0.5;, end = 1;
     } else if (ha == MOVE_TO_CUBE_TOP) {
         vx = 4 * bx2, vy = 4 * by2, vz = 4 * bz2, end = 1;
+        if (vz < 0) {
+            vz = max(state.get("vz") - 0.05, vz);
+        }
     } else if (ha == GRASP) {
         vx = 0, vy = 0, vz = 0.5, end = -1;
     } else {
         throw invalid_argument("Invalid high-level action label");
     }
 
-    // Make actions continuous
-    double dx = min(0.03, abs(vx / 4));
-    vx = min(max(vx, state.get("vx") - dx), state.get("vx") + dx);
-    double dy = min(0.03, abs(vy / 4));
-    vy = min(max(vy, state.get("vy") - dy), state.get("vy") + dy);
-    double dz = min(0.03, abs(vz / 4));
-    vz = min(max(vz, state.get("vz") - dz), state.get("vz") + dz);
-    double dend = min(0.03, abs(end / 4));
-    end = min(max(end, state.get("end") - dend), state.get("end") + dend);
-    
     return state.obs;
 }
 
 HA ASP_model(State state){
     HA ha = state.ha;
-    double bx1 = state.get("bx1") - state.get("x"),
-        by1 = state.get("by1") - state.get("y"),
-        bz1 = state.get("bz1") - state.get("z"), 
-        bx2 = state.get("bx2") - state.get("x"), 
-        by2 = state.get("by2") - state.get("y"), 
-        bz2 = state.get("bz2") - state.get("z"),
-        tx2 = state.get("tx2") - state.get("x"),
-        ty2 = state.get("ty2") - state.get("y"), 
-        tz2 = state.get("tz2") - state.get("z") + 0.01;
-    
-    if(ha == MOVE_TO_CUBE_BOTTOM && abs(bx1) < 0.002 && abs(by1) < 0.002 && abs(bz1) < 0.006) {
+    double bx1 = state.get("bx1"),
+        by1 = state.get("by1"),
+        bz1 = state.get("bz1"),
+        bx2 = state.get("bx2"),
+        by2 = state.get("by2"),
+        bz2 = state.get("bz2"),
+        tx2 = state.get("tx2"),
+        ty2 = state.get("ty2"),
+        tz2 = state.get("tz2");
+
+    if(ha == MOVE_TO_CUBE_BOTTOM && bz1 > -0.002) {
         return MOVE_TO_TARGET;
     }
-    if(ha == MOVE_TO_TARGET && abs(tx2) < 0.002 && abs(ty2) < 0.002 && abs(tz2) < 0.002) {
+    if(ha == MOVE_TO_TARGET && abs(tx2) < 0.002 && abs(ty2) < 0.002) {
         return LIFT;
     }
-    if(ha == LIFT && state.get("z") > 0.1 && state.get("bz2") < 0.03) {
+    if(ha == LIFT && flip(logistic(0.1, 100, state.get("z"))) && bz2 - bz1 < 0.03) {
         return MOVE_TO_CUBE_TOP;
     }
-    if(ha == MOVE_TO_CUBE_TOP && abs(bx2) < 0.002 && abs(by2) < 0.002 && abs(bz2) < 0.006) {
+    if(ha == MOVE_TO_CUBE_TOP && bz2 > -0.002) {
         return GRASP;
     }
-    if(ha == GRASP && state.get("z") > 0.1) {
+    if(ha == GRASP && flip(logistic(0.1, 100, state.get("z")))) {
         return MOVE_TO_TARGET;
     }
     return ha;
