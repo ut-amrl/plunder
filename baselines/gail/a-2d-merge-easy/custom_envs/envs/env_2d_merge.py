995399e1044884187ab0c2e282a702de95100140
Linus Zheng <linus.zheng@warriorlife.net>
2023-09-12 00:26:44 -0500

    revamp gail code for merge env but results are still not up to par

--- baselines/gail/a-2d-merge-easy/custom_envs/envs/env_2d_merge.py
+++ baselines/gail/a-2d-merge-easy/custom_envs/envs/env_2d_merge.py
@@ -5,9 +5,13 @@ from highway_env.utils import near_split, class_from_path
 import numpy as np
 from highway_env.road.road import Road, RoadNetwork
 from gym.utils import seeding
+from gym.spaces import Box
+from highway_env.envs.common.action import ContinuousAction
 
 
 # copied from merge.py --------------------------------------------------------
+steer_max = .3
+acc_max = 30
 lane_diff = 4
 KinematicObservation.normalize_obs = lambda self, df: df # Don't normalize values
 def _create_vehicles_custom(self) -> None:
@@ -30,7 +34,7 @@ def _create_vehicles_custom(self) -> None:
             for _ in range(others):
                 vehicle = other_vehicles_type.create_random(self.road, spacing=1.0 / self.config["vehicles_density"])
                 vehicle.randomize_behavior()
-                self.road.vehicles.append(vehicle)
+                self.road.vehicles.append(vehicle)               
 def laneFinder(y):
     return round(y / lane_diff)
 def classifyLane(obs):
@@ -62,20 +66,87 @@ def closestVehicles(env, obs, lane_class):
         closestRight[2] = obs[0][2] + lane_diff
     
     return (closestLeft, closestFront, closestRight)
+def special_act(self, action=None):
+  self.controlled_vehicle.act({
+      "steering": action[0] * steer_max,
+      "acceleration": action[1] * acc_max
+  })
+ContinuousAction.act = special_act
+
+
+
+
+# LA option calculations --------------------------------------------------------
+TURN_HEADING = 0.15 # Target heading when turning
+TURN_TARGET = 30 # How much to adjust when targeting a lane (higher = smoother)
+MAX_VELOCITY = 45 # Maximum velocity
+LA_COUNT = 4
+def motor_model(ha, data, last_la):
+    last_la = [last_la[0] * steer_max, last_la[1] * acc_max]
+    target_acc = 0.0
+    target_heading = 0.0
+
+    if ha == 0:
+        target_acc = MAX_VELOCITY - data[1]
+
+        target_y = laneFinder(data[1]) * 4
+        target_heading = np.arctan((target_y - data[1]) / TURN_TARGET)
+    elif ha == 1:
+        target_acc = data[12] - data[2]
+
+        target_y = laneFinder(data[1]) * 4
+        target_heading = np.arctan((target_y - data[1]) / TURN_TARGET)
+    elif ha == 2:
+        target_acc = -0.5
+        target_heading = -TURN_HEADING
+    else:
+        target_acc = -0.5
+        target_heading = TURN_HEADING
+
+    target_steer = target_heading - data[4]
+
+    if target_steer > last_la[0]:
+        target_steer = min(target_steer, last_la[0] + 0.08)
+    else:
+        target_steer = max(target_steer, last_la[0] - 0.08)
+
+    if target_acc > last_la[1]:
+        target_acc = min(target_acc, last_la[1] + 4)
+    else:
+        target_acc = max(target_acc, last_la[1] - 6)
+    res = [target_steer / steer_max, target_acc / acc_max]
+    res[0] = min(max(res[0], -1), 1)
+    res[1] = min(max(res[1], -1), 1)
+    return res
+def motor_model_options(proc_obs, last_la):
+    la_options = []
+    for ha in range(LA_COUNT):
+        la_options.extend(motor_model(ha, proc_obs, last_la))
+    return la_options
 
 
 # custom env ---------------------------------------------------------------
 class Env_2d_merge(HighwayEnv):
   obs_indices = []
   custom_config_set = False
+  last_la = [0,0]
+  max_t = -1
+  t = -1
   def __init__(self): 
     super().__init__()
     self.np_random = None
     self.seed()
-  def set_custom_config(self, custom_config, obs_indices):
+    temp1 = [-1, -1]
+    temp2 = [1, 1]
+    self.observation_space = Box(low=np.array([0, 0, 0, 0, 0]+temp1*5), 
+                                  high=np.array([600, 60, 600, 600, 600]+temp2*5), 
+                                  shape=(15,), dtype=np.float32)
+    self.action_space = Box(low=np.array(temp1), high=np.array(temp2), shape=(2,), dtype=np.float32)
+  def set_custom_config(self, custom_config, obs_indices, max_t):
     self.config.update(custom_config)
     self.obs_indices = obs_indices
     self.custom_config_set = True
+    self.max_t = max_t
 
   def raw_to_proc_obs(self, raw_obs):
     lane_class = classifyLane(raw_obs)
@@ -83,21 +154,30 @@ class Env_2d_merge(HighwayEnv):
     proc_obs = raw_obs[0][1:]
     for v in closest:
       proc_obs = np.append(proc_obs, v[1:])
-    proc_obs = [proc_obs[i] for i in self.obs_indices]
-    return proc_obs
+    proc_obs_pruned = [proc_obs[i] for i in self.obs_indices]
+    proc_obs_pruned.extend(self.last_la)
+    proc_obs_pruned.extend(motor_model_options(proc_obs, self.last_la))
+    return proc_obs_pruned
   def process_output(self, raw_out):
-    if self.custom_config_set:
-      raw_obs = raw_out[0]
-      proc_obs = self.raw_to_proc_obs(raw_obs[1:])
-      return tuple([proc_obs, 0, False, {}])
-    else:
-      return raw_out
+    raw_obs = raw_out[0]
+    proc_obs = self.raw_to_proc_obs(raw_obs)
+    terminated = self.t > self.max_t
+    return (proc_obs, 0, terminated, {})
   def step(self, action):
     raw_out = super().step(action)
+    self.last_la = action
+    self.t += 1
     return self.process_output(raw_out)
+  def _reset(self):
+    super()._reset()
+    self.last_la = [0,0]
+    self.t = 0
   def reset(self):
-    raw_out = super().reset()
-    return self.process_output(raw_out)
+    if self.custom_config_set:
+      raw_out = super().reset()
+      return self.process_output(raw_out)[0]
+    else:
+      return None
   def _create_vehicles(self):
     _create_vehicles_custom(self)
   def seed(self, seed=None):
--- baselines/gail/a-2d-merge-easy/train_2d.py
+++ baselines/gail/a-2d-merge-easy/train_2d.py
@@ -4,6 +4,12 @@ from imitation.util.util import make_vec_env
 import numpy as np
 import warnings
 warnings.filterwarnings("ignore")
+from imitation.algorithms.adversarial.gail import GAIL
+from stable_baselines3.ppo import MlpPolicy
+from stable_baselines3 import PPO
+from imitation.rewards.reward_nets import BasicRewardNet
+from imitation.util.networks import RunningNorm
+import gym
 
 
 # constants -------------------------------------------------------
@@ -17,14 +23,8 @@ _feature_column = ["x", "vx", "l_x", "f_x", "r_x"]
 _feature_indices = [0, 2, 5, 10, 15]
 _n_traj_train = 10
 _n_traj_test = 20
-_n_traj_tot = 1
-
-
-
-
-# globals -----------------------------------------------------------------
-_traj_all = None
-_venv = None
+_n_traj_tot = 30
+_n_timesteps = 74
 
 
 
@@ -34,6 +34,7 @@ _config1 = {
   "policy_frequency": 8,
   "lanes_count": 6,
   "initial_lane_id": 0,
+  'vehicles_count': 50,
   "observation": {
     'type': 'Kinematics',
     'vehicles_count': 10,
@@ -49,10 +50,11 @@ _config2 = {
   "policy_frequency": 8,
   "lanes_count": 8,
   "initial_lane_id": 0,
+  'vehicles_count': 50,
   "observation": {
     'type': 'Kinematics',
-    'vehicles_count': 50,
     'features': ['presence', 'x', 'y', 'vx', 'vy', 'heading'],
+    'vehicles_count': 50,
     'absolute': True
   },
   "action": {
@@ -68,33 +70,183 @@ def get_config_of_env_n(n):
 
 
 
-# get expert data -----------------------------------------------------------------
-def get_single_expert_traj(n):
-  data = pd.read_csv(_data_path+f"/data{n}.csv", skipinitialspace=True)
-  data = data[_ha_column + _la_column + _feature_column]
-  return data
+# LA option calculations --------------------------------------------------------
+TURN_HEADING = 0.15 # Target heading when turning
+TURN_TARGET = 30 # How much to adjust when targeting a lane (higher = smoother)
+MAX_VELOCITY = 45 # Maximum velocity
+LA_COUNT = 4
+lane_diff = 4
+steer_max = .3
+acc_max = 30
+def laneFinder(y):
+    return round(y / lane_diff)
+def motor_model(ha, data, last_la):
+    last_la = [last_la[0] * steer_max, last_la[1] * acc_max]
+    target_acc = 0.0
+    target_heading = 0.0
 
+    if ha == 0:
+        target_acc = MAX_VELOCITY - data[1]
+
+        target_y = laneFinder(data[1]) * 4
+        target_heading = np.arctan((target_y - data[1]) / TURN_TARGET)
+    elif ha == 1:
+        target_acc = data[12] - data[2]
+
+        target_y = laneFinder(data[1]) * 4
+        target_heading = np.arctan((target_y - data[1]) / TURN_TARGET)
+    elif ha == 2:
+        target_acc = -0.5
+        target_heading = -TURN_HEADING
+    else:
+        target_acc = -0.5
+        target_heading = TURN_HEADING
+
+    target_steer = target_heading - data[4]
+    if target_steer > last_la[0]:
+        target_steer = min(target_steer, last_la[0] + 0.08)
+    else:
+        target_steer = max(target_steer, last_la[0] - 0.08)
+
+    if target_acc > last_la[1]:
+        target_acc = min(target_acc, last_la[1] + 4)
+    else:
+        target_acc = max(target_acc, last_la[1] - 6)
+    res = [target_steer / steer_max, target_acc / acc_max]
+    res[0] = min(max(res[0], -1), 1)
+    res[1] = min(max(res[1], -1), 1)
+    return res
+def motor_model_options(proc_obs, last_la):
+    la_options = []
+    for ha in range(LA_COUNT):
+        la_options.extend(motor_model(ha, proc_obs, last_la))
+    return la_options
+def motor_model_options_all(proc_obs_arr, last_la_arr):
+  return np.array([motor_model_options(obs, la) for obs, la in zip(proc_obs_arr, last_la_arr)])
+
+
+
+
+# get expert data -----------------------------------------------------------------
+def get_single_expert_df(n):
+  return pd.read_csv(_data_path+f"/data{n}.csv", skipinitialspace=True)
+def get_single_expert_traj(n):
+  data = get_single_expert_df(n)
+  ha = data[_ha_column].to_numpy()
+  la = data[_la_column].to_numpy()
+  la = np.divide(la, [steer_max, acc_max])
+  features = data[_feature_column].to_numpy()
+  everything = data.to_numpy()
+  la = np.append(np.array([[0,0]]), la, axis=0)
+
+  obs = np.append(features[:-1], la[:-2], axis=1)
+  next_obs = np.append(features[1:], la[1:-1], axis=1)
+  acts = la[1:-1]
+
+  obs = np.append(obs, motor_model_options_all(everything[:-1], la[:-2]), axis=1)
+  next_obs = np.append(next_obs, motor_model_options_all(everything[1:], la[1:-1]), axis=1)
+
+  obs = obs[:_n_timesteps]
+  next_obs = next_obs[:_n_timesteps]
+  acts = acts[:_n_timesteps]
+  dones = np.array([False]*_n_timesteps)
+
+  return {
+    "obs": obs,
+    "next_obs": next_obs,
+    "acts": acts,
+    "dones": dones
+  }
 def get_all_expert_traj():
   return [get_single_expert_traj(i) for i in range(_n_traj_tot)]
 
-register(id=_env_id, entry_point=_custom_env_class_path)
 _traj_all = get_all_expert_traj()
+_traj_train = _traj_all[:_n_traj_train]
 
 
 
 
 # configure environments --------------------------------------------------
-_venv = make_vec_env(_env_id, n_envs=_n_traj_tot, rng=_rng)
-for i in range(_n_traj_tot):
-  _venv.env_method("set_custom_config", get_config_of_env_n(i), _feature_indices, indices=[i])
-  res = _venv.env_method("reset", indices=[i])
-  print(res[0][0])
-  for _ in range(9):
-    res = _venv.env_method("step", [0,0], indices=[i])
-    print(res[0][0])
-  print("-------------")
-  print(_traj_all[i][:10])
+register(id=_env_id, entry_point=_custom_env_class_path)
+_venv = make_vec_env(_env_id, n_envs=_n_traj_train, rng=_rng)
+for i in range(_n_traj_train):
+  _venv.env_method("set_custom_config", get_config_of_env_n(i), _feature_indices, _n_timesteps, indices=[i])
+_env_test = gym.make(_env_id)
+_env_test.set_custom_config(get_config_of_env_n(0), _feature_indices, _n_timesteps)
 
 
+
+
+# evaluation ---------------------------------------------------------
+def evaluate(model, trajectories):
+  for i, traj in enumerate(trajectories):
+    print(f"DATA ENV {i}")
+    for features, action in zip(traj["obs"], traj["acts"]):
+      predicted_action = list(model.predict(features)[0])
+      print(f"steer {predicted_action[0]*steer_max:.3f} --- {action[0]*steer_max:.3f}  \
+            --- acc {predicted_action[1]*acc_max:.3f} --- {action[1]*acc_max:.3f}")
+
+def sanity(model):
+  print("SANITY")
+  prev_obs = _env_test.reset()
+  print([f"{num:.3f}" for num in prev_obs])
+  for i in range(0,_n_timesteps):
+    predicted_action = model.predict(prev_obs)[0]
+    prev_obs = _env_test.step(predicted_action)[0]
+    print([f"{num:.3f}" for num in prev_obs])
+    if (_env_test.vehicle.crashed or not _env_test.vehicle.on_road):
+      print("CRASHED")
+      break
+
+def debug():
+  traj = _traj_all[0]
+  res = [_venv.env_method("reset", indices=[0])]
+  for i in range(0,_n_timesteps):
+    print("data: "+str([f"{num:.3f}" for num in traj["obs"][i]]))
+    print("siml: "+str([f"{num:.3f}" for num in res[0][0]]))
+    res = _venv.env_method("step", traj["acts"][i], indices=[0])
+  print("RESET")
+  res = [_venv.env_method("reset", indices=[0])]
+  for i in range(0,_n_timesteps):
+    print("data: "+str([f"{num:.3f}" for num in traj["obs"][i]]))
+    print("siml: "+str([f"{num:.3f}" for num in res[0][0]]))
+    res = _venv.env_method("step", traj["acts"][i], indices=[0])
+
+debug()
+
+# GAIL --------------------------------------------------------------
+_n_train_loops = 100000
+_n_train_steps = 15
+_learner = PPO(
+    env=_venv,
+    policy=MlpPolicy,
+    batch_size=_n_timesteps,
+    ent_coef=0.0002,
+    learning_rate=0.0005,
+    n_epochs=10,
+    gamma=.99,
+    n_steps=_n_timesteps
+)
+_reward_net = BasicRewardNet(
+    _venv.observation_space,
+    _venv.action_space,
+    normalize_input_layer=RunningNorm,
+)
+_gail_trainer = GAIL(
+    demonstrations=_traj_train,
+    demo_batch_size=_n_timesteps,                 # cons
+    gen_replay_buffer_capacity=_n_timesteps*2,    # cons
+    gen_train_timesteps=15,                       # gen per round
+    n_disc_updates_per_round=3,                   # disc per round
+    venv=_venv,
+    gen_algo=_learner,
+    reward_net=_reward_net,
+)
+
+for i in range(_n_train_loops):
+    print("LOOP # "+str(i))
+    _gail_trainer.train(_n_train_steps)           # total rounds = _n_train_steps / gen_train_timesteps
+    evaluate(_learner, _traj_all)
+    sanity(_learner)
 
 
