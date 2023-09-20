dab1a0491a2889b9c2aeb3b5542af29a704b9b6b
Jimmy Xin <jxin31415@gmail.com>
2023-09-19 23:27:28 -0500

    Attempt 5 at highway RL

--- 2D-highway-env/python-gen/train_rl.py
+++ 2D-highway-env/python-gen/train_rl.py
@@ -17,20 +17,20 @@ env.config['observation']={
 
 env.reset()
 
-# model = DQN.load("highway_dqn/model")
-# model.set_env(env)
+model = DQN.load("highway_dqn/model")
+model.set_env(env)
 
-model = DQN('MlpPolicy', env,
-              policy_kwargs=dict(net_arch=[256, 256]),
-              learning_rate=5e-4,
-              buffer_size=15000,
-              learning_starts=200,
-              batch_size=32,
-              gamma=0.8,
-              train_freq=1,
-              gradient_steps=1,
-              target_update_interval=50,
-              verbose=1,
-              tensorboard_log="highway_dqn/")
-model.learn(int(5e4))
+# model = DQN('MlpPolicy', env,
+#               policy_kwargs=dict(net_arch=[256, 256]),
+#               learning_rate=5e-4,
+#               buffer_size=15000,
+#               learning_starts=200,
+#               batch_size=32,
+#               gamma=0.8,
+#               train_freq=1,
+#               gradient_steps=1,
+#               target_update_interval=50,
+#               verbose=1,
+#               tensorboard_log="highway_dqn/")
+model.learn(int(1e5))
 model.save("highway_dqn/model")
