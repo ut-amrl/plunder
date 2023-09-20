7de90c3f9604c32dfb0bd3622a8f836697720bed
Jimmy Xin <jxin31415@gmail.com>
2023-09-20 18:33:29 -0500

    Attempt 7 at highway rl

--- 2D-highway-env/python-gen/run_rl.py
+++ 2D-highway-env/python-gen/run_rl.py
@@ -1,8 +1,21 @@
 import gymnasium as gym
 import highway_env
 from stable_baselines3 import DQN
+from highway_env.envs import ControlledVehicle, Vehicle
+from highway_env.envs.common.observation import KinematicObservation
 
 env = gym.make("highway-fast-v0", render_mode="rgb_array")
+KinematicObservation.normalize_obs = lambda self, df: df # Don't normalize values
+env.config['lanes_count']=4
+
+env.config['observation']={
+    'type': 'Kinematics',
+    'vehicles_count': 10,
+    'features': ['presence', 'x', 'y', 'vx', 'vy', 'heading'],
+    'absolute': True
+}
+
+env.reset()
 
 model = DQN.load("highway_dqn/model")
 model.set_env(env)
